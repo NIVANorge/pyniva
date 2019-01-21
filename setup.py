0b050c1c31bbcfed50e1f6481ab5fdcaf4034979
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2019-01-21 12:59:47 +0100

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='0.2.0',
 
     description="Python wrapper/API for interacting with NIVA's data platform",
 
