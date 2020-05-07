99e5d9f058e0b027bb97205030b294a4e67e6c44
RmStorm <roaldstorm@gmail.com>
2020-05-07 10:22:23 +0200

    Add license and fix package metadata

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='0.3.1',
 
     description="Python wrapper/API for interacting with NIVA's data platform",
 
@@ -19,11 +19,11 @@ setup(
     url='https://github.com/NIVANorge/pyniva',
 
     # Author details
-    author='Grunde Løvoll',
-    author_email='grunde.loevoll@niva.no',
+    author='Zofia Rudjord',
+    author_email='zofia.rudjord@niva.no',
 
     # Choose your license
-    license='Owned by NIVA',
+    license='MIT license',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -35,16 +35,14 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Data Access :: Time Series',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: Owned by NIVA http://www.niva.no/',
-        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'License :: MIT license',
+
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords='metadata timeseries data',
