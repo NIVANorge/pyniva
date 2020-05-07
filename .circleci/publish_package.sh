7a93f02be224426274c4d9719a0adb527b10599a
RmStorm <roaldstorm@gmail.com>
2020-05-07 10:09:31 +0200

    Add code to publish on pypi

--- .circleci/publish_package.sh
+++ .circleci/publish_package.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 set +x
+set -o nounset
 
 VERSION=$(python setup.py --version)
 python setup.py sdist
 curl -F package=@dist/pyniva-${VERSION}.tar.gz https://${FURY_TOKEN}@push.fury.io/niva/
+
+python setup.py sdist bdist_wheel
+
+pip install --user twine
+PATH=/home/circleci/.local/bin:$PATH
+twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} dist/*
