3d9b38e7c3b8550f1545c979357ee29cacd961a3
lisapro <elisaveta.pro@gmail.com>
2023-10-04 13:54:59 +0200

    fucntions from utils_pyniva fix #36

--- pyniva/request_dataframe.py
+++ pyniva/request_dataframe.py
@@ -9,14 +9,34 @@ from datetime import datetime, timedelta
 import re
 from dateutil.parser import parse
 from .thing import Vessel
-from .metaflow import META_HOST
+from .metaflow import PUB_META
+from .tsb import PUB_TSB
 
 # from .pyniva import Vessel, TimeSeries, token2header
 # from pyniva import META_HOST, PUB_META, TSB_HOST, PUB_TSB
 
 
-def get_available_parameters(platform_code, header):
-    measurements, tseries_paths = get_paths_measurements(platform_code, header)
+def get_paths_measurements(vessel_name, header, meta_host=None):
+    # Here we can directly get one object for the vessel we need without getting all of them as
+
+    if meta_host == None:
+        meta_host = PUB_META
+    print(f"{meta_host, header}")
+    print(vessel_name)
+    vessel_object = Vessel.get_thing(
+        meta_host=meta_host, header=header, params={"path": vessel_name}
+    )
+
+    print(f"vessel object {vessel_object}")
+    vessel_measurements = vessel_object.get_all_tseries(meta_host, header)
+    tseries_paths = [m.path for m in vessel_measurements]
+    return vessel_measurements, tseries_paths
+
+
+def get_available_parameters(platform_code, header, meta_host=None):
+    measurements, tseries_paths = get_paths_measurements(
+        platform_code, header, meta_host
+    )
     available_paths = [
         p for p in tseries_paths if "TEST" not in p and "SYSTEM" not in p
     ]
@@ -40,17 +60,27 @@ def get_available_ferrybox_sensor_group_parameters(platform_code):
 import pandas as pd
 
 
-def get_paths_measurements(vessel_name, header):
-    # Here we can directly get one object for the vessel we need without getting all of them as above
-    vessel_object = Vessel.get_thing(META_HOST, header, params={"path": vessel_name})
-    vessel_measurements = vessel_object.get_all_tseries(META_HOST, header)
-    tseries_paths = [m.path for m in vessel_measurements]
-    return vessel_measurements, tseries_paths
-
-
-def get_ship_data(vessel_name, param_paths, start_time, end_time, noqc, header, dt=0):
+def get_ship_data(
+    vessel_name: str,
+    param_paths: list,
+    start_time,
+    end_time,
+    noqc,
+    header,
+    dt=0,
+    pub_tsb=None,
+    meta_host=None,
+):
     print("Downloading data for ", vessel_name)
-    measurements, tseries_paths = get_paths_measurements(vessel_name)
+
+    if meta_host == None:
+        meta_host = PUB_META
+    if pub_tsb == None:
+        pub_tsb = PUB_TSB
+    measurements, tseries_paths = get_paths_measurements(
+        vessel_name, meta_host=meta_host, header=header
+    )
+
     df = pd.DataFrame()
     # make sure that all datasets have coordinates
     if f"{vessel_name}/gpstrack" not in param_paths:
@@ -60,19 +90,16 @@ def get_ship_data(vessel_name, param_paths, start_time, end_time, noqc, header, 
         try:
             tseries_idx = tseries_paths.index(path)
             var = measurements[tseries_idx].get_tseries(
-                PUB_TSB,
+                pub_tsb,
                 header=header,
                 noqc=noqc,
                 dt=dt,
                 start_time=start_time,
                 end_time=end_time,
             )
-
+            print(var)
             if not var.empty:
                 var = var.reset_index()
-                varname = var.columns[1]
-                if not "gpstrack" in path:
-                    var.rename(columns={varname: path}, inplace=True)
             else:
                 print("no data available")
 
@@ -83,7 +110,7 @@ def get_ship_data(vessel_name, param_paths, start_time, end_time, noqc, header, 
 
         except Exception as e:
             print(f"could not download {path, e}")
-            # print (df.head())
+
     if not df.empty:
         df = df.reset_index()
         df = df.sort_values(by="time", ascending=True)
--- tests/test_new_query_functions.py
+++ tests/test_new_query_functions.py
@@ -1,14 +1,49 @@
 import pytest
 import pyniva
-from pyniva.request_dataframe import get_ship_data, get_available_parameters
-from pyniva.metaflow import META_HOST
+from pyniva.request_dataframe import (
+    get_ship_data,
+    get_available_parameters,
+    get_paths_measurements,
+)
+from pyniva import token2header
+
+# from pyniva.metaflow import META_HOST
 
 
-def get_ship_data():
-    t = get_ship_data(vessel_name="FA")
-    print(t)
+def test_get_ship_data():
+    header = token2header("tests/niva-service-account.json")
+    param_paths = ["FA/FERRYBOX/C3/CDOM_FLUORESCENCE/RAW"]
+    start_time = "2023-02-01T11:00:00"
+    end_time = "2023-03-26T16:00:59"
+    t = get_ship_data(
+        vessel_name="FA",
+        param_paths=param_paths,
+        start_time=start_time,
+        end_time=end_time,
+        noqc=False,
+        header=header,
+        dt="P1D",
+        pub_tsb="https://ferrybox.t.niva.no/v1/tsb",
+        meta_host="https://ferrybox.t.niva.no/v1/metaflow",
+    )
+
+    assert not t.empty
 
 
 def test_get_available_parameters():
-    header = None
-    a = get_available_parameters("FA", header=header)
+    header = token2header("tests/niva-service-account.json")
+    a = get_available_parameters(
+        "FA", header=header, meta_host="https://ferrybox.t.niva.no/v1/metaflow"
+    )
+    assert "FA/FERRYBOX/C3/CDOM_FLUORESCENCE/RAW" in a
+
+
+def test_get_paths_measurements():
+    header = token2header("tests/niva-service-account.json")
+    vessel_measurements, tseries_paths = get_paths_measurements(
+        vessel_name="FA",
+        header=header,
+        meta_host="https://ferrybox.t.niva.no/v1/metaflow",
+    )
+
+    assert "FA/FERRYBOX/C3/CDOM_FLUORESCENCE/RAW/FROZEN_TEST" in tseries_paths
